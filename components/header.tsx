ce1d0af167dd42d0833bfc315a9b8e4378242a63
Klay4 <kevin.azemi@protonmail.ch>
2024-05-11 22:33:49 +0200

    Clerk added

--- components/header.tsx
+++ components/header.tsx
@@ -3,11 +3,21 @@ import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 
 import { ThemeToggle } from '@/components/theme-toggle'
 import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+import logo from '../public/logo.svg'
 
 export default function Header() {
   return (
     <header className='py-4'>
       <nav className='container flex items-center justify-between'>
+        <Image
+          src={logo}
+          width={50}
+          height={50}
+          alt="Picture of the author"
+        />
+        
         <ul className='flex gap-10 text-sm font-medium'>
           <li>
             <Link href='/'>Home</Link>
