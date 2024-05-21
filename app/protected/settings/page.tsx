768a6f63887043c646ae94ec9627dfb32e06ee0d
Kevin Azemi <kevin.azemi@protonmail.ch>
2024-05-21 10:07:54 +0200

    Settings page alerts + new api url env variable

--- app/protected/settings/page.tsx
+++ app/protected/settings/page.tsx
@@ -9,7 +9,6 @@ export default async function Page() {
   return (
     <section className='py-24'>
       <div className='container'>
-        <h1 className='text-3xl font-bold'>Edit Profile</h1>
         <EditProfile  userId={userId as string} />
       </div>
     </section>
